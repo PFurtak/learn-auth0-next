adddff27595a23ef1685c5a85c47fc738ce42b82
Patrick Furtak <patrick@patrickfurtak.com>
2021-03-11 13:12:19 -0500

    Loaded todos as props on home page render

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import NavBar from '../components/NavBar';
+import { table, minifyRecords } from './api/utils/Airtable';
 
-export default function Home() {
+export default function Home(initialTodos) {
+  console.log(initialTodos);
   return (
     <div>
       <Head>
@@ -14,4 +16,22 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const todos = await table.select({}).firstPage();
+    return {
+      props: {
+        initialTodos: minifyRecords(todos),
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        err: 'Something went wrong',
+      },
+    };
+  }
 }
